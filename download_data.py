fe8223d4b43d822688bea939c2750c99ad7f1522
KuangenZhang <kuangen.zhang@outlook.com>
2025-02-23 11:52:04 -0800

    Add download_data successfully.

--- download_data.py
+++ download_data.py
@@ -2,31 +2,53 @@ import wandb
 import os
 import pandas as pd
 import json
+import shutil
+from tqdm import tqdm 
 
-def wandb_json_to_csv(run_folder, json_name, csv_name, img_column_idx = 0):
+def wandb_table_to_csv(artifact_dir:str, table_name:str, csv_name:str, img_column_idx: int = 0):
     # Load the JSON file
-    with open(os.path.join(run_folder, json_name), "r") as f:
+    with open(os.path.join(artifact_dir, table_name), "r") as f:
         data = json.load(f)
 
+    assert "data" in data and "columns" in data
     # Extract the table data
-    if "data" in data and "columns" in data:
-        # Convert to pandas DataFrame
-        columns = data["columns"]
-        rows = data["data"]
-        for row in rows:
-            img_pred = row[img_column_idx]
-            if isinstance(img_pred, dict) and img_pred.get("_type") == "image-file":
-                row[img_column_idx] = os.path.join(run_folder, img_pred['path'])
-        df = pd.DataFrame(rows, columns=columns)
-        # Export to CSV
-        df.to_csv(os.path.join(run_folder, csv_name), index=False)
+    # Convert to pandas DataFrame
+    columns = data["columns"]
+    rows = data["data"]
+    for row in rows:
+        img_pred = row[img_column_idx]
+        if isinstance(img_pred, dict) and img_pred.get("_type") == "image-file":
+            row[img_column_idx] = os.path.join(artifact_dir, img_pred['path'])
+    df = pd.DataFrame(rows, columns=columns)
+    # Export to CSV
+    df.to_csv(os.path.join(artifact_dir, csv_name), index=False)
 
-run_id = "6ec5t9eu"
-run_folder_name = f"run-{run_id}-predictions_table:v0"
-run_folder = os.path.join("artifacts", run_folder_name)
-if not os.path.exists(run_folder):
+def download_files(entity: str, project: str, run_id: str = "6ec5t9eu", path_prefix: str = "predictions_table.table.json"):
+    run_folder_name = f"run-{run_id}-predictions_table:v0"
+    # if not os.path.exists(run_folder):
     run = wandb.init()
-    artifact = run.use_artifact(f'zhangkevin/LaneDetection2D/{run_folder_name}', type='run_table')
-    artifact_dir = artifact.download()
+    artifact = run.use_artifact(f'{entity}/{project}/{run_folder_name}', type='run_table')
+    artifact_dir = artifact.download(path_prefix=path_prefix)
+    return artifact_dir
 
-wandb_json_to_csv(run_folder, "predictions_table.table.json", "predictions_table.csv")
+
+def copy_src_imgs_to_cache(df, img_column_name, cache_img_column_name):
+    """
+    Copy images from the source path (df[img_column_name]) to the cache path (df[cache_img_column_name]).
+    
+    Args:
+        df (pd.DataFrame): The DataFrame containing image paths.
+        img_column_name (str): The column name for source image paths.
+        cache_img_column_name (str): The column name for cache image paths.
+    """
+    for src_path, cache_path in tqdm(zip(df[img_column_name], df[cache_img_column_name])):
+        # Ensure the source image exists
+        if not os.path.exists(src_path):
+            print(f"Warning: Source image not found: {src_path}")
+            continue
+        
+        # Create the cache directory if it doesn't exist
+        os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+        
+        # Copy the image from source to cache
+        shutil.copy(src_path, cache_path)
---
+++ download_diff.py
@@ -0,0 +1,88 @@
+""" 
+python download_diff.py -m laneatt_tusimple_resnet18_20250202_133647 laneatt_tusimple_resnet18_20250202_133647 -r 71dd2pnq gkhtc7x4 -d mean_iou
+
+SQL:
+SELECT img_cache_combined, mean_iou_diff, frame_id_0 as frame_id,  mean_iou_0 , mean_iou_1
+FROM current_df
+"""
+
+from download_data import download_files, wandb_table_to_csv, copy_src_imgs_to_cache
+import os
+import argparse
+import pandas as pd
+
+
+def combine_tables(df_list, cache_img_column_name: str, metrics_for_diff: str) -> pd.DataFrame:
+    assert len(df_list) == 2
+    df_combined = pd.concat(df_list, axis=1, keys=[0, 1])
+    df_combined.columns = [f"{col[1]}_{col[0]}" for col in df_combined.columns]
+    # Combine cache image paths from both DataFrames
+    df_combined[f"{cache_img_column_name}_combined"] = (
+        df_list[0][cache_img_column_name].astype(str) + "," + df_list[1][cache_img_column_name].astype(str)
+    )
+
+    # Calculate the difference between metrics in the two DataFrames
+    df_combined[f"{metrics_for_diff}_diff"] = (
+        df_list[1][metrics_for_diff] - df_list[0][metrics_for_diff]
+    )
+    return df_combined
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_versions", type=str, nargs='+', required=True, help="Versions the model")
+    parser.add_argument("-r", "--run_ids", type=str, nargs='+', required=True, help="Run ids of the test.")
+    parser.add_argument("-d", "--metrics_for_diff", type=str, required=True, help="The metrics to calculate diff")
+    parser.add_argument("-e", "--entity", type=str, default="zhangkevin", help="Entity name")
+    parser.add_argument("-p", "--project", type=str, default="LaneDetection2D", help="Project name")
+    parser.add_argument("-i", "--img_column_idx", type=int, default=0, help="Image column idx in the table")
+    parser.add_argument("-t", "--table_name", type=str, default="predictions_table.table.json", help="Table name")
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    cache_img_column_name = "img_cache"
+    print(args.model_versions)
+    df_list = []
+    for model_version, run_id in zip(args.model_versions, args.run_ids):
+        # download the tables
+        table_name = args.table_name
+        csv_name = "predictions_table.csv"
+        artifact_dir = download_files(args.entity, args.project, run_id, path_prefix=table_name)
+        
+        artifact_root_dir = os.path.dirname(artifact_dir)
+        model_artifact_dir = os.path.join(artifact_root_dir, model_version)
+        
+        csv_path = os.path.join(artifact_dir, csv_name)
+        
+        if os.path.exists(csv_path):
+            df = pd.read_csv(csv_path)
+        else:
+            wandb_table_to_csv(artifact_dir, table_name, csv_name, img_column_idx=args.img_column_idx)
+            df = pd.read_csv(csv_path)
+            img_column_name = df.columns[args.img_column_idx]
+            assert "frame_id" in df.columns, "Error: 'frame_id' column is missing in the DataFrame."
+            df[cache_img_column_name] = df["frame_id"].apply(lambda x: os.path.join(model_artifact_dir, "media", f"{x}.png"))
+            df.to_csv(csv_path, index=False)
+        
+            if not os.path.exists(model_artifact_dir):
+                download_files(args.entity, args.project, run_id, path_prefix="media/")
+                # copy imgs from df[img_column_name] to df[cache_img_column_name]
+                copy_src_imgs_to_cache(df, img_column_name, cache_img_column_name)
+
+        df_list.append(df)
+    
+    print(len(df_list))
+     
+    df_combined = combine_tables(df_list, cache_img_column_name, args.metrics_for_diff)
+    print(f"Downloaded table to {csv_path}")
+    df_combined_path = os.path.join(artifact_root_dir, "_".join(args.model_versions) + ".csv")
+    df_combined.to_csv(df_combined_path, index=False)
+    print(f"Saved combined dataframe to {df_combined_path}")
+    
+
+if __name__ == "__main__":
+    main()
+    
+
+
